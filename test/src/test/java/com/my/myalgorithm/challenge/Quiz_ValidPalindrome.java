42590542921c085c14ab23ffc2bf2cf963b7c274
Boy Wang <boyw165@gmail.com>
2017-10-05 00:04:19 +0800

    Add quiz, "Valid Palindrome".

--- test/src/test/java/com/my/myalgorithm/challenge/Quiz_ValidPalindrome.java
+++ test/src/test/java/com/my/myalgorithm/challenge/Quiz_ValidPalindrome.java
@@ -20,6 +20,7 @@
 
 package com.my.myalgorithm.challenge;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -39,9 +40,54 @@ import org.junit.Test;
 public class Quiz_ValidPalindrome {
 
     @Test
-    public void answer() throws Exception {
+    public void test() throws Exception {
+        Assert.assertEquals(true, isPalindrome("madam"));
+        Assert.assertEquals(true, isPalindrome("A man, a plan, a canal: Panama"));
+        Assert.assertEquals(false, isPalindrome("race a car"));
+        Assert.assertEquals(false, isPalindrome("9;8'4P?X:1Q8`dOfJuJXD6FF,8;`Y4! YBy'Wb:ll;;`;\"JI0c2uvD':!LAk:s\"!'0.!2B55.T1VI?00Du?1,l``RFsc?Y?9vD5 K'3'1b!N8hn:'l. R:9:o`m1r.M2mrJ?`Wjv1`G6i6G`1vjW`?Jrm2M.r1m`o:::R .l':nh8N!b1'3'K 5Dv9?Y?csFR``l,1?uD00?IV1T.55B2!.0'!\"s:kAL!:'Dvu2c0IJ\";`;;ll9bW'yBY !4Y`;8,FF6DXJuJfOd`8Q1:X?P4'8;9"));
     }
 
     ///////////////////////////////////////////////////////////////////////////
     // Protected / Private Methods ////////////////////////////////////////////
+
+    /**
+     * Beats 94% on LeetCode.
+     */
+    private boolean isPalindrome(String s) {
+        if (s == null) return false;
+        if (s.length() <= 1) return true;
+
+        // Time complexity: O(n)
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+
+            if (!isAlphanumericLetter(left)) {
+                ++i;
+                continue;
+            }
+            if (!isAlphanumericLetter(right)) {
+                --j;
+                continue;
+            }
+
+            if (Character.toLowerCase(left) !=
+                Character.toLowerCase(right)) {
+                return false;
+            }
+
+            ++i;
+            --j;
+        }
+
+        return true;
+    }
+
+    private boolean isAlphanumericLetter(char c) {
+        return (c >= '0' && c <= '9') ||
+               (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z');
+    }
 }
