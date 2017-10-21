1bd59f306dce81dfabb7afe69cf57659135c9d03
Boy Wang <boyw165@gmail.com>
2017-10-21 12:58:59 +0800

    Update comment of "Sum Of Two Integers".

--- test/src/test/java/com/my/myalgorithm/challenge/QuizEasy_SumOfTwoIntegers.java
+++ test/src/test/java/com/my/myalgorithm/challenge/QuizEasy_SumOfTwoIntegers.java
@@ -41,8 +41,21 @@ import org.junit.Test;
 public class QuizEasy_SumOfTwoIntegers {
 
     @Test
-    public void test() throws Exception {
+    public void allPositives() throws Exception {
         Assert.assertEquals(3, getSum(1, 2));
+        Assert.assertEquals(9, getSum(5, 4));
+    }
+
+    @Test
+    public void allNegatives() throws Exception {
+        Assert.assertEquals(-4, getSum(-1, -3));
+        Assert.assertEquals(-12, getSum(-5, -7));
+    }
+
+    @Test
+    public void onePositiveAndOneNegative() throws Exception {
+        Assert.assertEquals(4, getSum(-1, 5));
+        Assert.assertEquals(145, getSum(152, -7));
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -52,34 +65,66 @@ public class QuizEasy_SumOfTwoIntegers {
         if (a == 0) return b;
         if (b == 0) return a;
 
-        // Suppose we're working with 8 bit quantities, sum of 3 plus 7:
-        //
-        //          carry =      111
-        //              3 = 0000 0011
-        //              7 = 0000 0111
-        //
+        // Tips:
         // Use AND operator to find out what bits producing carry bits,
         //
-        //              3 = 0000 0011
-        //          AND 7 = 0000 0111
-        //     carry bits = 0000 0011
+        //              3 = 0011
+        //          AND 7 = 0111
+        //     carry bits = 0011
         //
         // Use XOR operator to find the binary addition without carries,
         //
-        //             3 = 0000 0011
-        //         XOR 7 = 0000 0111
-        //  add no carry = 0000 0100
+        //             3 = 0011
+        //         XOR 7 = 0111
+        //  add no carry = 0100
 
-        int sum = a;
-        int carry = b;
-        while (carry != 0) {
-            int newCarry = (sum & carry) << 1;
+        // Suppose we're working with 8 bit quantities, sum of 3 plus 7:
+        // Goal:  0011
+        //      + 0111
+        //
+        //
+        //        0011         0011
+        //    AND 0111     XOR 0111
+        //    --------     --------
+        //    <<1 0011         0100 ---. sum without carries
+        //    --------                 |
+        //        0110 ----------------+ carries are still present...
+        //                             |
+        //                             |
+        // Goal:  0110 <---------------'
+        //      + 0100
+        //
+        //        0110         0110
+        //    AND 0100     XOR 0100
+        //    --------     --------
+        //    <<1 0100         0010 ---. sum without carries
+        //    --------                 |
+        //        1000 ----------------+ carries are still present...
+        //                             |
+        //                             |
+        // Goal:  1000 <---------------'
+        //      + 0010
+        //
+        //        1000         1000
+        //    AND 0010     XOR 0010
+        //    --------     --------
+        //    <<1 0000         1010 ---> sum without carries
+        //    --------
+        //        0000 ----------------> NO carries anymore, so the answer is
+        //                               the sum without carries.
+        //
+        // Ans:   1010
 
-            sum = sum ^ carry;
-            carry = newCarry;
+        int sumWithoutCarries = a ^ b;
+        int carries = (a & b) << 1;
+        while (carries != 0) {
+            int oldSum = sumWithoutCarries;
+
+            sumWithoutCarries = oldSum ^ carries;
+            carries = (oldSum & carries) << 1;
         }
 
-        return sum;
+        return sumWithoutCarries;
     }
 
     /**
